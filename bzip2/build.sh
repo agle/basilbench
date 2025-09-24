3747b47a80e2f59b5ff1879af5a29c0920b07aa0
am <a.michael1@uq.edu.au>
2025-09-24 11:11:00 +1000

    nothing

--- bzip2/build.sh
+++ bzip2/build.sh
@@ -4,7 +4,7 @@
 eval $(../docker-helper.sh env)
 export CC="$GCC"
 export CFLAGS="$CFLAGS -O2"
-../liftmake.sh -c bzip2.c >> buildlog
+../liftmake.sh -c bzip2.c
 
 export CFLAGS="-O0"
-../liftmake.sh -c bzip2.c -b bzip2_O0 >> buildlog
+../liftmake.sh -c bzip2.c -b bzip2_O0
